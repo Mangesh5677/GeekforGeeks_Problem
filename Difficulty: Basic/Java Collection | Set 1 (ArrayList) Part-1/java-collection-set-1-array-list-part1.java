df71fa1b15adebd18c0b93100a503c026d55623b
Mangesh shinde <149881322+Mangesh5677@users.noreply.github.com>
2026-07-03 21:10:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java Collection | Set 1 (ArrayList) Part-1/java-collection-set-1-array-list-part1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static ArrayList<Integer> solve(int N, ArrayList<Integer> A, int Q, ArrayList<Integer> Query) {
+
+        if (Q == 1) {
+            int p = Query.get(0);
+            int r = Query.get(1);
+
+            A.add(p, r);
+            return A;
+        } else {
+            int p = Query.get(0);
+
+            ArrayList<Integer> ans = new ArrayList<>();
+            ans.add(A.lastIndexOf(p));
+
+            return ans;
+        }
+    }
+}

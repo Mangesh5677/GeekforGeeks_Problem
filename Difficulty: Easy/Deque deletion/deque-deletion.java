486726d84babca472b51e584573f34b6ac580c27
Mangesh shinde <149881322+Mangesh5677@users.noreply.github.com>
2026-07-10 20:27:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Deque deletion/deque-deletion.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public static void eraseAt(ArrayDeque<Integer> deq, int X) {
+        ArrayList<Integer> list = new ArrayList<>(deq);
+        list.remove(X);
+
+        deq.clear();
+        deq.addAll(list);
+    }
+
+    public static void eraseInRange(ArrayDeque<Integer> deq, int start, int end) {
+        if (start == end)
+            return;
+
+        ArrayList<Integer> list = new ArrayList<>(deq);
+
+        for (int i = end - 1; i >= start; i--) {
+            list.remove(i);
+        }
+
+        deq.clear();
+        deq.addAll(list);
+    }
+
+    public static void eraseAll(ArrayDeque<Integer> deq) {
+        deq.clear();
+    }
+}

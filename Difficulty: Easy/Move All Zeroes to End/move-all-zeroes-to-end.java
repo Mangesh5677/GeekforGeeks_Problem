28954fa0953e3bad87c1651a086093ca55c2bd49
Mangesh shinde <149881322+Mangesh5677@users.noreply.github.com>
2026-07-13 12:32:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Move All Zeroes to End/move-all-zeroes-to-end.java
@@ -0,0 +1,15 @@
+class Solution {
+    void pushZerosToEnd(int[] arr) {
+        int index = 0;
+
+        // Move all non-zero elements to the front
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                int temp = arr[index];
+                arr[index] = arr[i];
+                arr[i] = temp;
+                index++;
+            }
+        }
+    }
+}

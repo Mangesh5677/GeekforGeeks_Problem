48b5c6189365d4961a5fd81eece93a3244424453
Mangesh shinde <149881322+Mangesh5677@users.noreply.github.com>
2026-07-13 12:54:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First Repeating Element/first-repeating-element.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+class Solution {
+    public static int firstRepeated(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (map.get(arr[i]) > 1) {
+                return i + 1;   
+            }
+        }
+        return -1;
+    }
+}

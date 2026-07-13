eef1fd522e7712514a65d0ea21f78d6ff467e79a
Mangesh shinde <149881322+Mangesh5677@users.noreply.github.com>
2026-07-13 20:25:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Total count/total-count.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int totalCount(int k, int[] arr) {
+
+        int count = 0;
+
+        for (int num : arr) {
+            count += (num + k - 1) / k;
+        }
+
+        return count;
+    }
+}

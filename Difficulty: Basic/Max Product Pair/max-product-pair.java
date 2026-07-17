213f4640cc0a29af4a04c167becbc98885220d44
Mangesh shinde <149881322+Mangesh5677@users.noreply.github.com>
2026-07-17 19:23:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Max Product Pair/max-product-pair.java
@@ -0,0 +1,29 @@
+class Solution {
+    public static int maxProduct(int[] arr) {
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for (int num : arr) {
+            // Two largest
+            if (num >= max1) {
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max2 = num;
+            }
+
+            // Two smallest
+            if (num <= min1) {
+                min2 = min1;
+                min1 = num;
+            } else if (num < min2) {
+                min2 = num;
+            }
+        }
+
+        return Math.max(max1 * max2, min1 * min2);
+    }
+}

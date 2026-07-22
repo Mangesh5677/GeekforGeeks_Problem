bed4260903415125634f496ad6e47e8d67f3f816
Mangesh shinde <149881322+Mangesh5677@users.noreply.github.com>
2026-07-22 20:30:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Four Elements/four-elements.java
@@ -0,0 +1,16 @@
+class Compute {
+    boolean find4Numbers(int A[], int n, int X) {
+        for (int i = 0; i < n - 3; i++) {
+            for (int j = i + 1; j < n - 2; j++) {
+                for (int k = j + 1; k < n - 1; k++) {
+                    for (int l = k + 1; l < n; l++) {
+                        if (A[i] + A[j] + A[k] + A[l] == X) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}

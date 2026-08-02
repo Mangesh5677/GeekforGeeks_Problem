3a200f8dae4141cc2bda928371944867d6d8a72e
Mangesh shinde <149881322+Mangesh5677@users.noreply.github.com>
2026-08-02 21:36:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Repeated Character/repeated-character.java
@@ -0,0 +1,20 @@
+class Solution {
+    public char firstRep(String s) {
+        
+        int[] freq = new int[26];
+
+        // Count frequency of each character
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+
+        // Find first repeating character
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] > 1) {
+                return s.charAt(i);
+            }
+        }
+
+        return '#';
+    }
+}
